8ac7edfa3dfa74b855862ebac122956c97429ca6
Akshat SHARMA <akshat25ai004@satiengg.in>
2024-07-20 10:08:00 +0530

    chore: Improve visibility of "Added to cart" message

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,4 @@
+let addedMessageTimeoutId;
 let productHtml = "";
 
 products.forEach((product) => {
@@ -42,7 +43,7 @@ products.forEach((product) => {
 
                   <div class="product-spacer"></div>
 
-                  <div class="added-to-cart">
+                  <div class="added-to-cart js-added-to-cart-${product.id}">
                     <img src="images/icons/checkmark.png">
                     Added
                   </div>
@@ -82,6 +83,24 @@ document.querySelectorAll(".js-addto-cart").forEach((button) => {
       cartQuantity += item.quantity;
     });
     document.querySelector('.js-cartquantity').innerHTML = cartQuantity;
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add('add-visible')
+    setTimeout(() => {
+      // Check if a previous timeoutId exists. If it does,
+      // we will stop it.
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+    
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('add-visible');
+      }, 2000);
+    
+      // Save the timeoutId so we can stop it later.
+      addedMessageTimeoutId = timeoutId;
+    }, 0);
+    });
   });
-});
 
