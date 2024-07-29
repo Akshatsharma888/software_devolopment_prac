ded3b1b0f2a019ceea14cd65de1251bbfa004539
Akshat SHARMA <akshat25ai004@satiengg.in>
2024-07-29 19:13:21 +0530

    refactor: Improve code readability and efficiency in money.js, checkout.js, products.js, and deliveryoptions.js

--- javascript-amazon-project/scripts/checkout/ordersummary.js
+++ javascript-amazon-project/scripts/checkout/ordersummary.js
@@ -6,28 +6,18 @@ import {
   updateQuantity,
   updateDeliveryOption,
 } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products ,getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryoptions.js";
+import { deliveryOptions ,getDeliveryOptions} from "../../data/deliveryoptions.js";
 
 export function renderOrderSummary() {
   let cartSummaryHtml = "";
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
-    let matchingProduct;
-    products.forEach((product) => {
-      if (productId === product.id) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
     const deliveryOptionId = cartItem.deliveryOptionsId;
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOptions(deliveryOptionId);
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
     const daysString = deliveryDate.format("dddd, MMMM D");
