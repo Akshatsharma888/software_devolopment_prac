133e52a91483b9ace5b79dafd19ff259a91632d2
Akshat SHARMA <akshat25ai004@satiengg.in>
2024-08-06 16:40:17 +0530

    refactor: Update products.js to use fetch API for loading product data and remove commented code

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -82,9 +82,31 @@ class Appliance extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        } else if (productDetails.type === "appliances") {
+          return new Appliance(productDetails);
+        }
+        return new Product(productDetails);
+      });
+      console.log("loaded products."); // Moved inside the load event listener
+    });
+    return promise;
+}
+// loadProductsFetch().then(()=>{
+//   console.log('next step')
+// });
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', () => {
+  xhr.addEventListener("load", () => {
     products = JSON.parse(xhr.responseText).map((productDetails) => {
       if (productDetails.type === "clothing") {
         return new Clothing(productDetails);
@@ -93,10 +115,10 @@ export function loadProducts(fun) {
       }
       return new Product(productDetails);
     });
-    console.log('loaded products.'); // Moved inside the load event listener
+    console.log("loaded products."); // Moved inside the load event listener
     fun();
   });
-  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.open("GET", "https://supersimplebackend.dev/products");
   xhr.send();
 }
 /*
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../scripts/checkout/ordersummary.js"; 
 import { renderPaymentSummary } from "../scripts/checkout/paymentsummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 import '../data/backend-prac.js';
 
@@ -8,11 +8,7 @@ import '../data/backend-prac.js';
 // thats why we use promises to keep our code flat intead using callbacks.
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
