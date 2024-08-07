11a424e0ded26e4357f8bc67b43d243861229423
Akshat SHARMA <akshat25ai004@satiengg.in>
2024-08-07 19:00:21 +0530

    refactor: Update checkout.js to use async/await for loading products and rendering order/payment summary

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -6,15 +6,26 @@ import '../data/backend-prac.js';
 
 // with the help of promise we can wait for each step to finish and then resolve to move to next step using then.
 // thats why we use promises to keep our code flat intead using callbacks.
+// Async await is shortcut for promises.
+// Async just make a function return its promise.
 
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
+async function loadPage(){
+  await loadProductsFetch();
+  await  new Promise((resolve) => {
     loadCart(() => {
       resolve();
     });
-  })
-]).then(() => {
+  });
   renderOrderSummary();
   renderPaymentSummary();
-});
+}
+loadPage();
+
+
+// Promise.all([
+//   loadProductsFetch(),
+
+// ]).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
